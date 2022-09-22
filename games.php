c44232db15b6deb6b496ed8503fcec9689655631
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 11:19:42 +0530

    Update games.php

--- games.php
+++ games.php
@@ -18,65 +18,32 @@
 <div class = "table">
 <h1>Genres and their respective ID's:</h1>
 <?php
-$host    = "localhost";
-$user    = "root";
-$pass    = "";
-$db_name = "GameRaptor";
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = new mysqli($host, $user, $pass, $db_name);
-$result = $conn->query("SELECT * FROM genre limit 100");
-$query = array();
-while($query[] = mysqli_fetch_assoc($result));
-array_pop($query);
-echo '<table border="1" style="width:100%;background-color:black;" >';
-echo '<tr>';
-foreach($query[0] as $key => $value) {
-    echo '<td>';
-    echo $key;
-    echo '</td>';
-}
-echo '</tr>';
-foreach($query as $row) {
+    $host    = "localhost";
+    $user    = "root";
+    $pass    = "";
+    $db_name = "GameRaptor";
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $conn = new mysqli($host, $user, $pass, $db_name);
+    $result = $conn->query("SELECT * FROM genre limit 100");
+    $query = array();
+    echo '<table border="1" style="width:100%;background-color:black;" >';
     echo '<tr>';
-    foreach($row as $column) {
-        echo '<td style = "text-align:left;">';
-        echo $column;
+    foreach($query[0] as $key => $value) {
+        echo '<td>';
+        echo $key;
         echo '</td>';
     }
     echo '</tr>';
-}
-echo '</table>';;
-                ?>
-                <h2>Games</h2>
-<?php
-$host    = "localhost";
-$user    = "root";
-$pass    = "";
-$db_name = "GameRaptor";
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = new mysqli($host, $user, $pass, $db_name);
-$result = $conn->query("SELECT * FROM game limit 100");
-$query = array();
-while($query[] = mysqli_fetch_assoc($result));
-array_pop($query);
-echo '<table border="1" style="width:100%;background-color:black;" >';
-echo '<tr>';
-foreach($query[0] as $key => $value) {
-    echo '<td>';
-    echo $key;
-    echo '</td>';
-}
-echo '</tr>';
-foreach($query as $row) {
-    echo '<tr>';
-    foreach($row as $column) {
-        echo '<td style = "text-align:left;">';
-        echo $column;
-        echo '</td>';
+    foreach($query as $row) {
+        echo '<tr>';
+        foreach($row as $column) {
+            echo '<td style = "text-align:left;">';
+            echo $column;
+            echo '</td>';
+        }
+        echo '</tr>';
     }
-    echo '</tr>';
-}
-echo '</table>';;
-                ?>
+    echo '</table>';
+    ?>
 </body>
 </html>
