784b59be3777a93242abe152394b72f309bb290b
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 11:27:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 	</head>
 	<body>
 		<div class = "container">
-		<form action="index.php" method="POST">
-		<h2><p><img src="img/logo.jpg">Welcome to GameRaptor</h2>
-		<p style = "text-align:center;"><label><u><b>Registration Form</b></u></label><br>
+			<form action="index.php" method="POST">
+			<h2><p><img src="img/logo.jpg">Welcome to GameRaptor</h2>
+			<p style = "text-align:center;"><label><u><b>Registration Form</b></u></label><br>
 			<input type="text" name="username" placeholder="Username" required="required"  /> <br>
 			<input type="password" name="password" placeholder="Password" required="required" /> <br/>
-			<input type="text" name="Gender" placeholder="Gender"   /> <br>
-			<input type="text" name="Email" placeholder="Email"  /> <br>
+			<input type="text" name="Gender" placeholder="Gender"/> <br>
+			<input type="text" name="Email" placeholder="Email"  required = "required"/> <br>
 			Enter your DOB<br><input type="date" name="dob" placeholder="Date Of Birth" value=""min="1997-01-01" max="2030-12-31"/><br>
-			<input type="submit" value="Register"/><br>
+			<input type="submit" value="Register" /><br>
 			<nav>
 			<a href="login.php">Already have an account? Login..</a><br>
 			</nav>
-		</form>	
+			</form>	
 		</div>	
 		<?php
 		$username = '';
@@ -29,21 +29,21 @@
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$username = $_POST['username'];
 			$password = $_POST['password'];
-			$Birthday = $_POST['dob'];
-			$gender = $_POST['Gender'];
+			$Birthday = (empty($_POST['dob']))? '9999/99/99' : $_POST['dob'];
+			$gender = (empty($_POST['Gender']))? 'Unknown' : $_POST['gender'];
 			$email = $_POST['Email'];
 			$db = mysqli_connect("localhost","root","","gameraptor");
 			if($db==false){
-				die("ERROR: Could not connect. " . mysqli_connect_error());
+				die("ERROR: Could not connect -> " . mysqli_connect_error());
 			}
-			$query = mysqli_query($db,"Select * from users"); //Query the users table
-			while($row = mysqli_fetch_array($query)) //display all rows from query
+			$query = mysqli_query($db,"Select username from users");
+			while($row = mysqli_fetch_array($query))
 			{
-				$table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
-				if($username == $table_users) // checks if there are any matching fields
+				$table_users = $row['username'];
+				if($username == $table_users) 
 				{
-					$bool = false; // sets bool to false
-					Print '<script>alert("Username has been taken! please retry");</script>'; //Prompts the user
+					$bool = false;
+					Print '<script>alert("Username has been taken! please retry");</script>';
 					Print '<script>window.location.assign("index.php");</script>';
 				}
 			}
