3e25defb41efe1036b8be3e0fc593bb7a8b35866
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 11:23:33 +0530

    Update home.php

--- home.php
+++ home.php
@@ -5,10 +5,8 @@
         <link rel="stylesheet" href="home.css">
     </head>
     <?php
-    session_start(); //starts the session
-    if($_SESSION['user']){ // checks if the user is logged in  
-    }
-    else{
+    session_start();
+    if(!$_SESSION['user']){
         header("location: index.php"); // redirects if user is not logged in
     }
     $user = $_SESSION['user']; //assigns user value
@@ -28,26 +26,26 @@
                 </nav>
             </div>
             <div class = "bodybox">
-            <h1 > Welcome, <?php Print "$user"?></h1>
-            <a href = "Account.php">Account Settings</a>
-            <a href = "games.php">Games</a>
-            <a href = "news.php">News</a>
-            <a href = "mygames.php">My Profile</a>
+            	<h1 > Welcome, <?php Print "$user"?></h1>
+            	<a href = "Account.php">Account Settings</a>
+            	<a href = "games.php">Games</a>
+            	<a href = "news.php">News</a>
+            	<a href = "mygames.php">My Profile</a>
             </div>
-                <h2>Games</h2>
-<div class="row">
-  <div class="column">
-  <a href="hzd.php"><img src="img/HZD.jpg"></a>
-    <p>Horizon Zero Dawn</p></br></a>
-    <a href="GOW.php"><img src="img/GOW.jpg"></br></br></a>
-    <p>God Of War</p>
-  </div>
-  <div class = "column">
-  <a href="RDR.php"><img src="img/RDR.png"></a>
-    <p>Red Dead Redemption</p>
-    <a href="spd.php"><img src="img/Spiderman.jpg"></a>
-    <p>Spiderman</p>
-  </div>
-</div>
+            <h2>Games</h2>
+	    <div class="row">
+  		<div class="column">
+  			<a href="hzd.php"><img src="img/HZD.jpg"></a>
+    			<p>Horizon Zero Dawn</p></br></a>
+    			<a href="GOW.php"><img src="img/GOW.jpg"></br></br></a>
+    			<p>God Of War</p>
+ 		</div>
+  		<div class = "column">
+  			<a href="RDR.php"><img src="img/RDR.png"></a>
+    			<p>Red Dead Redemption</p>
+    			<a href="spd.php"><img src="img/Spiderman.jpg"></a>
+    			<p>Spiderman</p>
+  		</div>
+	    </div>
 	</body>
 </html>
