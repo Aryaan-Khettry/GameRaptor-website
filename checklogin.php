e84c65e3ef659be52b1f53c398426cc06c8a5cb3
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 11:13:10 +0530

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,4 +1,5 @@
 <?php
+//Verify user login
     session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -6,31 +7,18 @@
     $bool = true;
 
     $db = mysqli_connect("localhost","root","","gameraptor");
-    $query = mysqli_query($db,"Select * from users WHERE username='$username'"); // Query the 
-                                                                            // users table
-    $exists = mysqli_num_rows($query); //Checks if username exists
+    $query = mysqli_query($db,"Select * from users WHERE username='$username'");
+    $exists = mysqli_num_rows($query);
     $table_users = "";
     $table_password = "";
-    if($exists > 0) //IF there are no returning rows or no existing username
+    if($exists > 0) 
     {
-       while($row = mysqli_fetch_assoc($query)) // display all rows from query
-       {
-          $table_users = $row['username'];     // the first username row is 
-                                               // passed on to $table_users, 
-                                               // and so on until the query is finished
-          $table_password = $row['password'];  // the first password row is passed on 
-                                               // to $table_password, and so on 
-                                               // until the query is finished
-       }
-       if(($username == $table_users) && ($password == $table_password)) // checks if there 
-                                                                  // are any matching fields
-       {
-          if($password == $table_password)
-          {
-             $_SESSION['user'] = $username;    // set the username in a session. 
-                                               // This serves as a global variable
-             header("location: home.php");     // redirects the user to the authenticated 
-                                               // home page
+       $row = mysqli_fetch_assoc($query) 
+          $table_users = $row['username']; 
+          $table_password = $row['password'];  
+        // Setting username as a session variable
+             $_SESSION['user'] = $username; 
+             header("location: home.php"); 
           }
        }
        else
@@ -41,7 +29,7 @@
     }
     else
     {
-        Print '<script>alert("Incorrect username!");</script>';        // Prompts the user
-        Print '<script>window.location.assign("login.php");</script>'; // redirects to login.php
+        Print '<script>alert("Incorrect username!");</script>';     
+        Print '<script>window.location.assign("login.php");</script>'; 
     }
 ?>
